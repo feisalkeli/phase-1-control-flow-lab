40448b6a6f9043a67123e9e9075ff71191ef5364
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-03-14 11:03:01 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function scuberGreetingForFeet(ride) {
   // Write your code here!
   if (ride <= 400) {
     return `This one is on me!`;
-  } else if (ride >= 2000) {
-    return "I will gladly take your thirty bucks.";
   } else if (ride >= 2500) {
-    return "No";
+    return "No can do.";
+  } else {
+    return "I will gladly take your thirty bucks.";
   }
   debugger;
 }
@@ -23,14 +23,13 @@ function switchOnCharmFromTip(tip) {
   // Write your code here!
   if (tip === "generous") {
     return `Thank you so much.`;
-  } else if (tip !== "generous") {
+  } else if (tip === "not as generous") {
     return `Thank you.`;
-  } else if ((tip = "anything else")) {
-    return `Bye`;
+  } else {
+    return `Bye.`;
   }
   // switch (tip) {
   //   case "Tip":
   //     "Thank you so much.";
   // }
 }
-switchOnCharmFromTip("get ready");
